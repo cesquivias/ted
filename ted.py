229c3bc104c2798c65a68d0192ff6f5a451d7fdc
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-04-14 13:39:49 -0700

    Turn off echoing

--- ted.py
+++ ted.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
 import sys
+import termios
+
+def enable_raw_mode():
+    fd = sys.stdin.fileno()
+    raw = termios.tcgetattr(fd)
+    raw[3] = raw[3] & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSAFLUSH, raw)
+
 
 if __name__ == '__main__':
+    enable_raw_mode()
+
     while True:
         c = sys.stdin.read(1)
         if c is None:
