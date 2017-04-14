2d046bd48b25ffaf51694164c1a0a266f0478f2d
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-04-14 13:45:44 -0700

    Disable raw mode at exit

--- ted.py
+++ ted.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
+import atexit
 import sys
 import termios
 
+ORIG_TERMIOS = None
+
 def enable_raw_mode():
+    global ORIG_TERMIOS
+
     fd = sys.stdin.fileno()
+    ORIG_TERMIOS = termios.tcgetattr(fd)
     raw = termios.tcgetattr(fd)
     raw[3] = raw[3] & ~termios.ECHO
     termios.tcsetattr(fd, termios.TCSAFLUSH, raw)
+    atexit.register(disable_raw_mode)
+
+def disable_raw_mode():
+    fd = sys.stdin.fileno()
+    termios.tcsetattr(fd, termios.TCSAFLUSH, ORIG_TERMIOS)
 
 
 if __name__ == '__main__':
