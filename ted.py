c86937955d5818bf52c3020776a33bb810759703
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-04-17 11:50:24 -0700

    Refactor keyboard input

--- ted.py
+++ ted.py
@@ -9,11 +9,29 @@ import sys
 import termios
 import tty
 
+def ctrl(key):
+    return chr(ord(key) & 0x1f)
+
 def enable_raw_mode(fd):
     original_attrs = termios.tcgetattr(fd)
     atexit.register(functools.partial(termios.tcsetattr, fd, termios.TCSAFLUSH,
                                       original_attrs))
     tty.setraw(fd)
+
+def read_key(fd):
+    try:
+        return os.read(fd, 1)
+    except OSError as err:
+        if err.errno == errno.EAGAIN:
+            return None
+        else:
+            raise
+
+def process_key_press(fd):
+    c = read_key(fd)
+
+    if c == ctrl('q'):
+        sys.exit(0)
 
 
 if __name__ == '__main__':
@@ -21,18 +39,4 @@ if __name__ == '__main__':
     enable_raw_mode(fd)
 
     while True:
-        try:
-            c = os.read(fd, 1)
-        except OSError as err:
-            if err.errno == errno.EAGAIN:
-                c = None
-            else:
-                raise
-        if not c:
-            continue
-        if curses.ascii.iscntrl(c):
-            print '%d\r' % ord(c)
-        else:
-            print "%d ('%c')\r" % (ord(c), c)
-        if c == 'q':
-            break
+        process_key_press(fd)
