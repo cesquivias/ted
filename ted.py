0fddd9ec7517879e7927db4592bae484f0d50292
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-04-17 10:45:17 -0700

    Use tty for convenient raw mode

--- ted.py
+++ ted.py
@@ -3,37 +3,22 @@
 import atexit
 import curses.ascii
 import errno
+import functools
 import os
 import sys
 import termios
+import tty
 
-ORIG_TERMIOS = None
-
-def enable_raw_mode():
-    global ORIG_TERMIOS
-
-    fd = sys.stdin.fileno()
-    ORIG_TERMIOS = termios.tcgetattr(fd)
-
-    raw = termios.tcgetattr(fd)
-    raw[0] = raw[0] & ~(termios.IXON | termios.ICRNL)
-    raw[1] = raw[1] & ~(termios.OPOST)
-    raw[3] = raw[3] & ~(termios.ECHO | termios.ICANON | termios.ISIG | termios.IEXTEN)
-    raw[6][termios.VMIN] = 0
-    raw[6][termios.VTIME] = 1
-
-    termios.tcsetattr(fd, termios.TCSAFLUSH, raw)
-
-    atexit.register(disable_raw_mode)
-    return fd
-
-def disable_raw_mode():
-    fd = sys.stdin.fileno()
-    termios.tcsetattr(fd, termios.TCSAFLUSH, ORIG_TERMIOS)
+def enable_raw_mode(fd):
+    original_attrs = termios.tcgetattr(fd)
+    atexit.register(functools.partial(termios.tcsetattr, fd, termios.TCSAFLUSH,
+                                      original_attrs))
+    tty.setraw(fd)
 
 
 if __name__ == '__main__':
-    fd = enable_raw_mode()
+    fd = sys.stdin.fileno()
+    enable_raw_mode(fd)
 
     while True:
         try:
